fe50bcc6da38bafb5dbe236660e4a7e70549fafa
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-09-27 00:50:38 +0100

    Update SenseScript.py

--- SenseScript.py
+++ SenseScript.py
@@ -8,7 +8,7 @@ humidity = round(sense.get_humidity())
 pressure = round(sense.get_pressure())
 message = 'Temp is %d C Humidity is %d percent Pressure is %d mbars' %(temp,humidity,pressure)
 
-while true:
+while True:
     sense.show_message(message, scroll_speed=(0.08),text_colour=[200,0,200],back_colour=[0,0,200])
     sense.clear
     sleep(5)
