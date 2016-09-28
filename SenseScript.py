ed1f7c658fc0478773e05afe770bf8f61938259d
Craig Hissett <CraigHissett@users.noreply.github.com>
2016-09-28 22:15:12 +0100

    Update SenseScript.py

--- SenseScript.py
+++ SenseScript.py
@@ -8,7 +8,19 @@ humidity = round(sense.get_humidity())
 pressure = round(sense.get_pressure())
 message = 'Temp is %d C Humidity is %d percent Pressure is %d mbars' %(temp,humidity,pressure)
 
-while True:
+def GetValues(Requested):
+    if Requested =="Pressure":
+        return pressure
+    elif Requested=="Humidity":
+        return humidity
+    elif Requested=="Temp":
+        return temp
+
+def DisplayValues():
     sense.show_message(message, scroll_speed=(0.08),text_colour=[200,0,200],back_colour=[0,0,200])
     sense.clear
+
+    
+while True:
+    DisplayValues
     sleep(5)
